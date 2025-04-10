5df1b2abd7338638b5a694f8ce36c7938d853ba2
prakkritti1999 <prakriti.guptta@gmail.com>
2025-04-10 12:10:59 +0530

    removed sensitive data

--- NotificationService/src/main/java/com/NotificationService/NotificationServiceApplication.java
+++ NotificationService/src/main/java/com/NotificationService/NotificationServiceApplication.java
@@ -2,8 +2,10 @@ package com.NotificationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:secret.properties")
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
